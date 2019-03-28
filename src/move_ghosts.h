1be5138e26ca497faf18ad2dfe9ad248a16d2fe6
Samuel Pfalzer <43767211+samuel-pfalzer@users.noreply.github.com>
2019-03-28 23:23:38 +0100

    Delete move_ghosts.h

--- src/move_ghosts.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOVE_GHOSTS_H
-#define MOVE_GHOSTS_H
-
-#include "defines.h"
-
-void move_ghosts(char board[FIELD_HEIGHT][FIELD_WIDTH], int nr_ghosts, ghost_info ghost_array[4], int* pacman_caught, int moves);
-
-#endif
